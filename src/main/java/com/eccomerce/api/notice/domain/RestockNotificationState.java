cdd7fb3f0d01d7c089d0358b65c6a7f84e1448e3
younghyun.j <zerohyun.j@gmail.com>
2024-10-14 13:26:55 +0900

    [feat] 재입고 알림 진행 상태, 회원 재입고 알림 활성화 여부 Enum 추가

---
+++ src/main/java/com/eccomerce/api/notice/domain/RestockNotificationState.java
@@ -0,0 +1,8 @@
+package com.eccomerce.api.notice.domain;
+
+public enum RestockNotificationState {
+    IN_PROGRESS,
+    CANCELLED_BY_SOLD_OUT,
+    CANCELLED_BY_ERROR,
+    COMPLETED,
+}
---
+++ src/main/java/com/eccomerce/api/notice/domain/UserRestockSubState.java
@@ -0,0 +1,6 @@
+package com.eccomerce.api.notice.domain;
+
+public enum UserRestockSubState {
+    ACTIVE,
+    INACTIVE,
+}
